bb641f1ff18860b8908e9be547ba222adf15f6c4
komal-sd <141730275+komal-sd@users.noreply.github.com>
2026-02-20 11:53:06 +0000

    Restructure to production-grade layout

---
+++ week3/day1/app/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip3 install -r requirements.txt
+nohup python3 app.py > app.log 2>&1 &
--- week3/day1/user-data.sh
+++ week3/day1/user-data.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-yum update -y
-yum install git python3 -y
+sleep 30
+
+sudo yum update -y
+sudo yum install git python3 -y
 
 cd /home/ec2-user
-git clone https://github.com/komal-sd/jan26-devops-bootcamp-.git
+git clone https://github.com/komal-sd/jan26-devops-bootcamp.git
 
-cd jan26-devops-bootcamp/week3/day1
-pip3 install -r requirements.txt
-
-nohup python3 app.py > app.log 2>&1 &
+cd jan26-devops-bootcamp/week3/day1/app
+chmod +x run.sh
+./run.sh
